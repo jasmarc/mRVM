05d1c349e8d62759cc813eacb1168e915112a33b
Jason Marcell <jasonmarcell@gmail.com>
2011-05-18 01:19:40 -0400

    changes to run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 VERBOSITY=${1-1}
 
-# valgrind \
-#   --track-origins=yes \
-#   --leak-check=full \
-#   --show-reachable=yes \
+valgrind \
+  --track-origins=yes \
+  --leak-check=full \
+  --show-reachable=yes \
 ./bin/mRVM \
   -k GAUSSIAN \
   -v $VERBOSITY \
-  --train   ./data/iris.train.dat \
-  --labels  ./data/iris.labels.dat \
-  --test    ./data/iris.test.dat \
-  --answers ./data/iris.answers.dat \
-  --out     ./data/iris.out.dat \
+  --train   ./data/dev.train.dat \
+  --labels  ./data/dev.labels.dat \
+  --test    ./data/dev.test.dat \
+  --answers ./data/dev.answers.dat \
+  --out     ./data/dev.out.dat \
   --param   2 \
   --tau     0.000001 \
   --upsilon 0.000001
+
+# 1st Probit Kernel Regression, Kernel-based with multi-nomial probit likilhood
+# 2nd Relevence Vector Machine (mRVM2), multi-class, top-down approach
+# 3rd Relevence Vector Machine (mRVM1), multi-class, bottom-up approach
